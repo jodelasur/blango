53a4c748edc3b7cb69b6e22d5c78314ec223dbec
Jodel Asur <jodelasur@gmail.com>
2022-08-23 23:46:56 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -43,4 +43,5 @@ if settings.DEBUG:
         path("accounts/", include("django_registration.backends.activation.urls")),
         path("accounts/profile/", blango_auth.views.profile, name="profile"),
         path("accounts/", include("allauth.urls")),
+        path("api/v1/", include("blog.api_urls")),
     ]

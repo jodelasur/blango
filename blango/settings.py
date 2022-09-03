e9ad17eef01dad2ab0926cd0577c568fe6046bb3
Jodel Asur <jodelasur@gmail.com>
2022-09-03 01:53:23 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -59,6 +59,7 @@ class Dev(Configuration):
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
       'rest_framework',
+      'rest_framework.authtoken',
   ]
 
   MIDDLEWARE = [
@@ -202,6 +203,14 @@ class Dev(Configuration):
   ACCOUNT_AUTHENTICATION_METHOD = "email"
 
   SITE_ID = 1
+
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+  }
 
 class Prod(Dev):
   DEBUG = False
